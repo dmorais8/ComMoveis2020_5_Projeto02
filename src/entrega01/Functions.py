49c1f6eea8343a0ea6b84e8263d04ebb266dc50f
David Morais <morais.david@live.com>
2020-07-06 23:19:56 -0300

    Testes iniciais

---
+++ src/entrega01/Functions.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+__author__ = "David Morais"
+
+
+class Pessoa:
+
+    def __init__(self):
+        self.nome = ""
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, nome):
+        self.nome = nome
---
+++ src/entrega01/main.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+__author__ = "David Morais"
+__copyright__ = "Copyright 2020, Projeto 01, Comunicacoes Moveis"
+__credits__ = ["David Morais"]
+__maintainer__ = "David Morais"
+__email__ = "moraisdavid8@gmail.com"
+__status__ = "Development"
+__version__ = "0.0.1-SNAPSHOT"
+
+# External imports
+import numpy as np
+from matplotlib import pyplot as plt
+
+#Project imports
+from src.entrega01.Functions import Pessoa
+
+if __name__ == "__main__":
+
+    pessoa = Pessoa()
+    pessoa.set_nome("David")
+    print(pessoa.get_nome())
