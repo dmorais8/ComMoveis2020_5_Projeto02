5ed26ae1b02eca3de11f990cb20c92cf3968f93f
David Morais <morais.david@live.com>
2020-07-07 01:20:13 -0300

    :seedling: Iniciando pratica 02

--- src/practices/pratica02.py
+++ src/practices/pratica02.py
@@ -1,5 +1,19 @@
 # coding: utf-8
 __author__ = "David Morais"
 
+# Lib imports
 import numpy as np
+from matplotlib import pyplot as plt
 
+# CONSTANTS
+n_bits = 100            # Número de bits
+T = 50                  # Tempo de símbolo
+Ts = 2                  # Tempo de símbolo em portadora única
+K = T/Ts                # Número de subportadoras independentes
+N = 2*K                 # N pontos da IDFT
+
+if __name__ == '__main__':
+
+    data_in = np.random.rand(1, n_bits)[0]
+    data_in = np.sign(data_in - .5)
+    print()
