ca4185f2a37c436f4817143efcecd3ac17168aea
David Morais <morais.david@live.com>
2020-07-07 15:18:03 -0300

    :tada: Finalizando pratica 02

--- src/practices/pratica02.py
+++ src/practices/pratica02.py
@@ -11,9 +11,34 @@ T = 50                  # Tempo de símbolo
 Ts = 2                  # Tempo de símbolo em portadora única
 K = T/Ts                # Número de subportadoras independentes
 N = 2*K                 # N pontos da IDFT
+PI = np.pi
+i = 1j
 
 if __name__ == '__main__':
 
-    data_in = np.random.rand(1, n_bits)[0]
+    data_in = np.random.rand(1, n_bits)
     data_in = np.sign(data_in - .5)
-    print()
+    data_in_matrix = data_in.reshape((25, 4))
+
+    seq16qam = 2 * data_in_matrix[:, 0]+data_in_matrix[:, 1] + 1j * (2 * data_in_matrix[:, 2] + data_in_matrix[:, 3])
+    X = np.append(seq16qam, np.conj(seq16qam))
+
+    xn = np.zeros((1, int(N)), dtype=complex)
+    for n in range(int(N)):
+        for k in range(int(N)):
+            xn[0, n] = xn[0, n] + 1 / np.sqrt(N) * X[k] * np.exp(i * 2 * PI * n * k / N)
+
+    xt = np.zeros((1, int(T+1)), dtype=complex)
+    for t in range(int(T+1)):
+        for k in range(int(N)):
+            xt[0, t] = xt[0, t] + 1 / np.sqrt(N) * X[k] * np.exp(i * 2 * PI * k * t / T)
+
+    plt.plot(np.abs(xn[0]), label="x(t)")
+    markerline, stemlines, baseline = plt.stem(np.abs(xt[0]), use_line_collection=True, linefmt='red',
+                                               markerfmt='go', label="x_n")
+    markerline.set_markerfacecolor('none')
+    plt.title('Sinais OFDM')
+    plt.legend(loc="upper left")
+    plt.xlabel('Tempo')
+    plt.show()
+
